6758e7df7e9d961d530351158cb042bb82bf1b0d
Nolan Freeman Abbott <nolan@thenofreeman.com>
2025-04-15 05:13:02 -0500

    fix deprecated fn

--- lua/symbols-outline/providers/nvim-lsp.lua
+++ lua/symbols-outline/providers/nvim-lsp.lua
@@ -9,7 +9,7 @@ local function getParams()
 end
 
 function M.hover_info(bufnr, params, on_info)
-  local clients = vim.lsp.buf_get_clients(bufnr)
+  local clients = vim.lsp.get_clients({ bufnr = bufnr })
   local used_client
 
   for id, client in pairs(clients) do
@@ -38,7 +38,7 @@ end
 
 -- probably change this
 function M.should_use_provider(bufnr)
-  local clients = vim.lsp.buf_get_clients(bufnr)
+  local clients = vim.lsp.get_clients({ bufnr = bufnr })
   local ret = false
 
   for id, client in pairs(clients) do
